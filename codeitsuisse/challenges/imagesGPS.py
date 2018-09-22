76799e9ab77221e3b5bec6d84f0230ab9cde4bc6
CurryBay <caleb.mah.2009@vjc.sg>
2018-09-22 09:42:10 +0800

    Added imageGPS endpoint

---
+++ codeitsuisse/challenges/imagesGPS.py
@@ -0,0 +1,17 @@
+import exifread
+def evaluate(data):
+    result = []
+    for input in data:
+        image_path = input.get("path")
+        image = open(image_path, 'rb')
+        tags = exifread.process_file(f)
+        results.append(tags)
+    return results
+
+tests = [
+    {path: "/images/sample1.jpg"},
+    {path: "/images/sample2.jpg"},
+    {path: "/images/sample3.jpg"},
+    {path: "/images/sample4.jpg"},
+    {path: "/images/sample5.jpg"}
+]
