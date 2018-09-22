3083ba0689390474939ad424ec4fc4651b57f8c7
icycoldx3 <adnakerh@gmail.com>
2018-09-22 13:55:03 +0800

    prime sums recurrsion

--- codeitsuisse/challenges/prime-sum.py
+++ codeitsuisse/challenges/prime-sum.py
@@ -2,66 +2,129 @@ import math
 from itertools import count, islice
 
 def evaluate(inputValue):
+    #inputValue = inputValue["input"]
     def isPrime(n):
         return n > 1 and all(n%i for i in islice(count(2), int(math.sqrt(n)-1)))
-
-    inputValue = inputValue["input"]
-    #print ("inputValue = ", inputValue)
     if isPrime(inputValue):
         return [inputValue]
 
-    primes = []
-    for possiblePrime in range(2, inputValue):
-        # Assume number is prime until shown it is not.
-        is_Prime = True
-        for num in range(2, possiblePrime):
-            if possiblePrime % num == 0:
-                is_Prime = False
-        if is_Prime:
-            primes.append(possiblePrime)
+    #print ("inputValue = ", inputValue)
+
+    def generate_primes(n):
+        primes = []
+        for possiblePrime in range(2, inputValue):
+            # Assume number is prime until shown it is not.
+            is_Prime = True
+            for num in range(2, possiblePrime):
+                if possiblePrime % num == 0:
+                    is_Prime = False
+            if is_Prime:
+                primes.append(possiblePrime)
+        return primes
 
     #primes.append(1)
-    primes = sorted(primes, reverse = True)
-    # if not primes:
-    #     print ("list is empty")
+
+    primes = generate_primes(inputValue)
+    # primes = sorted(primes, reverse = True)
     tempPrimes = primes[:]
-    # if not tempPrimes:
-    #     print ("list is empty")
+    discarded = []
+    included = []
     currVal = inputValue
-    currList = []
     n = 0
+    i = 0
 
-    while currVal != 0:
-        currVal = inputValue
-        #print ("tempPrimes = ",  tempPrimes)
-        if not tempPrimes:
-            return None
-        for i in tempPrimes:
-            if currVal - i == 0:
-                currList.append(i)
-                print ("currList = ",  currList)
-                return currList
-            elif currVal - i > 0:
-                currVal = currVal - i
-                currList.append(i)
-        tempPrimes.pop(0)
-        currList = []
+    def sumOfPrimes (n, listOfPrimes, included):
+        if n == 0:
+            return True
+        if n < 0 or len(listOfPrimes) == 0:
+            return False
+        if n - listOfPrimes[-1] >= 0:
+            included.append(listOfPrimes[-1])
+        return sumOfPrimes(n - listOfPrimes[-1], listOfPrimes[:-1], included) or sumOfPrimes(n, listOfPrimes[:-1], included)
 
-    # def sum_of_primes(value):
-    #     primes = prime_sieve(value)
-    #     lo = 0
-    #     hi = len(primes) - 1
-    #     while lo <= hi:
-    #         prime_sum = primes[lo] + primes[hi]
-    #         if prime_sum < value:
-    #             lo += 1
-    #         else:
-    #             if prime_sum == value:
-    #                 yield primes[lo], primes[hi]
-    #             hi -= 1
-    # return
+        # currVal = n - listOfPrimes[0]
+        # if currVal == 0:
+        #     goodPrimes.append(listOfPrimes[0])
+        #     return goodPrimes
+        # elif n > 0:
+        #     print ("n > 0")
+        #     currVal = n - listOfPrimes[0]
+        #     goodPrimes.append(listOfPrimes[0])
+        #     print(goodPrimes)
+        #     return sum_of_primes(currVal, goodPrimes)
+
+
+    sumOfPrimes(inputValue, primes, included)
+
+    return included
+
+
+
+
+    # def sumOfPrimes (n, listOfPrimes, discarded, included):
+    #     if n == 0:
+    #         return True
+    #     if n < 0 or len(listOfPrimes) == 0:
+    #         return False
+    #     if sumOfPrimes(inputValue, listOfPrimes[:-1], discarded, included):
+    #         discarded.append(listOfPrimes[-1])
+    #         print("discarded: ", discarded)
+    #     if sumOfPrimes(inputValue - listOfPrimes[-1], listOfPrimes[:-1], discarded, included):
+    #         included.append(listOfPrimes[-1])
+    #         print("included: ", included)
+    #     return sumOfPrimes(inputValue, listOfPrimes[:-1], discarded, included) or sumOfPrimes(inputValue - listOfPrimes[-1], listOfPrimes[:-1], discarded, included)
+    #
+    # if sumOfPrimes(inputValue, primes, discarded, included):
+    #     print ("help")
+    #     return included
+    # else:
+    #     print ("that's wrong")
+    #     return None
+
+
+
+    # while currVal != 0:
+    #     currVal = inputValue
+    #     if not tempPrimes:
+    #         return None
+    #     for i in tempPrimes:
+    #         if currVal - i == 0:
+    #             currList.append(i)
+    #             print ("currList = ",  currList)
+    #             return currList
+    #         elif currVal - i > 0:
+    #             currVal = currVal - i
+    #             currList.append(i)
+    #     tempPrimes.pop(0)
+    #     currList = []
+
+
+
+    # primes = generate_primes(inputValue)
+    # primes = sorted(primes, reverse = True)
+    # tempPrimes = primes[:]
+    # currVal = inputValue
+    # currList = []
+    # n = 0
+    # i = 0
+    # while currVal != 0:
+    #     currVal = inputValue
+    #     #print ("tempPrimes = ",  tempPrimes)
+    #     if not tempPrimes:
+    #         return None
+    #     for i in tempPrimes:
+    #         if currVal - i == 0:
+    #             currList.append(i)
+    #             print ("currList = ",  currList)
+    #             return currList
+    #         elif currVal - i > 0:
+    #             currVal = currVal - i
+    #             currList.append(i)
+    #     tempPrimes.pop(0)
+    #     currList = []
+
 
 tests = [
-    10000
+    100
 
 ]
