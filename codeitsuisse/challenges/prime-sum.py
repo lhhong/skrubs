1fd5f53132c889e0296dd64b5279dcdee7a6d3b2
CurryBay <caleb.mah.2009@vjc.sg>
2018-09-22 09:37:30 +0800

    prime-sum

--- codeitsuisse/challenges/prime-sum.py
+++ codeitsuisse/challenges/prime-sum.py
@@ -1,8 +1,7 @@
-def evaluate(inputValue):
-    return inputValue + 1
+def evaluate(data):
+    inputValue = data.get("input")
+    primes = [inputValue%i != 0 for i in range(2, int(inputValue**0.5)+1)]
+    return primes
 
-tests = [
-    1,
-    2,
-    3
-]
+
+tests = [{ "input": 19 }]
