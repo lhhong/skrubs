fcdb496ea624e9b7f391e77ada6974dfc20ad1de
lhhong <heok.hong.low@gmail.com>
2018-09-22 16:10:53 +0800

    message broadcast

---
+++ codeitsuisse/challenges/broadcaster-sl-message-broadcast.py
@@ -0,0 +1,48 @@
+def evaluate(inputVal):
+    data = inputVal['data']
+    graph = {}
+    reverseGraph = {}
+    allNodes = set()
+    for d in data:
+        node = d.split('->')
+        allNodes.add(node[0])
+        allNodes.add(node[1])
+        if node[0] not in graph:
+            graph[node[0]] = set()
+        if node[1] not in graph:
+            graph[node[1]] = set()
+        graph[node[0]].add(node[1])
+
+    S = []
+
+    def dfs(graph, start, visited, fn):
+        visited.add(start)
+        for n in graph[start] - visited:
+            dfs(graph, n, visited, fn)
+        fn(start)
+        return visited
+
+
+    all_visited = set()
+    for n in allNodes:
+        if n not in all_visited:
+            all_visited.union(dfs(graph, n, all_visited, lambda start: S.append(start)))
+
+    ans = []
+    all_visited = set()
+    while len(S) > 0:
+        n = S.pop()
+        ans.append(n)
+        def removal(s):
+            if s in S:
+                S.remove(s)
+        all_visited.union(dfs(graph, n, all_visited, lambda s: removal(s)))
+
+    return ans
+
+tests = [
+    {
+        "data" : [ "A->B" , "A->C" , "B->D" , "E->F" ]
+    }
+]
+
--- codeitsuisse/challenges/skill-tree.py
+++ codeitsuisse/challenges/skill-tree.py
@@ -1,5 +1,4 @@
 def evaluate(inputVal):
-    print(inputVal)
     skills = inputVal['skills']
     boss = inputVal['boss']
 
@@ -42,8 +41,13 @@ def evaluate(inputVal):
         if include != -1:
             for poss in include:
                 if newSkills[index]["require"] == None or newSkills[index]["require"] in poss[0]:
-                    poss[0].append(newSkills[index]["name"])
-                    poss[1] += newSkills[index]["points"]
+                    if newSkills[index]['name'] in poss[0]:
+                        print(poss[0])
+                        print(newSkills[index]['name'])
+                        print('')
+                    ll = poss[0][:]
+                    ll.append(newSkills[index]["name"])
+                    score = poss[1] + newSkills[index]["points"]
                     res.append(poss)
 
         if exclude != -1:
@@ -118,5 +122,6 @@ tests = [
                             "require": "Inverted takedown"
                           }
                 ]
-    }
+    },
+    {'boss': {'offense': 210}, 'skills': [{'name': 'skill-22', 'offense': 13, 'points': 14, 'require': 'skill-21'}, {'name': 'skill-50', 'offense': 14, 'points': 12, 'require': 'skill-49'}, {'name': 'skill-51', 'offense': 18, 'points': 13, 'require': 'skill-50'}, {'name': 'skill-49', 'offense': 10, 'points': 11, 'require': 'skill-48'}, {'name': 'skill-38', 'offense': 112, 'points': 24, 'require': 'skill-37'}, {'name': 'skill-52', 'offense': 22, 'points': 14, 'require': 'skill-51'}, {'name': 'skill-34', 'offense': 78, 'points': 23, 'require': 'skill-33'}, {'name': 'skill-36', 'offense': 94, 'points': 24, 'require': 'skill-35'}, {'name': 'skill-19', 'offense': 6, 'points': 8, 'require': 'skill-18'}, {'name': 'skill-27', 'offense': 33, 'points': 19, 'require': 'skill-26'}, {'name': 'skill-56', 'offense': 46, 'points': 17, 'require': 'skill-55'}, {'name': 'skill-26', 'offense': 28, 'points': 18, 'require': 'skill-25'}, {'name': 'skill-2', 'offense': 6, 'points': 8, 'require': 'skill-1'}, {'name': 'skill-44', 'offense': 175, 'points': 26, 'require': 'skill-43'}, {'name': 'skill-24', 'offense': 20, 'points': 16, 'require': 'skill-23'}, {'name': 'skill-6', 'offense': 17, 'points': 15, 'require': 'skill-5'}, {'name': 'skill-16', 'offense': 48, 'points': 24, 'require': 'skill-15'}, {'name': 'skill-29', 'offense': 44, 'points': 20, 'require': 'skill-28'}, {'name': 'skill-42', 'offense': 153, 'points': 26, 'require': 'skill-41'}, {'name': 'skill-4', 'offense': 11, 'points': 12, 'require': 'skill-3'}, {'name': 'skill-18', 'offense': 4, 'points': 5, 'require': None}, {'name': 'skill-32', 'offense': 63, 'points': 22, 'require': 'skill-31'}, {'name': 'skill-15', 'offense': 45, 'points': 23, 'require': 'skill-14'}, {'name': 'skill-48', 'offense': 8, 'points': 10, 'require': 'skill-47'}, {'name': 'skill-23', 'offense': 16, 'points': 15, 'require': 'skill-22'}, {'name': 'skill-8', 'offense': 23, 'points': 18, 'require': 'skill-7'}, {'name': 'skill-7', 'offense': 20, 'points': 17, 'require': 'skill-6'}, {'name': 'skill-5', 'offense': 14, 'points': 14, 'require': 'skill-4'}, {'name': 'skill-10', 'offense': 29, 'points': 20, 'require': 'skill-9'}, {'name': 'skill-45', 'offense': 2, 'points': 3, 'require': None}, {'name': 'skill-3', 'offense': 8, 'points': 10, 'require': 'skill-2'}, {'name': 'skill-28', 'offense': 38, 'points': 19, 'require': 'skill-27'}, {'name': 'skill-35', 'offense': 86, 'points': 23, 'require': 'skill-34'}, {'name': 'skill-9', 'offense': 26, 'points': 19, 'require': 'skill-8'}, {'name': 'skill-20', 'offense': 8, 'points': 10, 'require': 'skill-19'}, {'name': 'skill-17', 'offense': 51, 'points': 24, 'require': 'skill-16'}, {'name': 'skill-54', 'offense': 33, 'points': 16, 'require': 'skill-53'}, {'name': 'skill-37', 'offense': 103, 'points': 24, 'require': 'skill-36'}, {'name': 'skill-41', 'offense': 142, 'points': 25, 'require': 'skill-40'}, {'name': 'skill-47', 'offense': 5, 'points': 8, 'require': 'skill-46'}, {'name': 'skill-14', 'offense': 41, 'points': 22, 'require': 'skill-13'}, {'name': 'skill-21', 'offense': 10, 'points': 12, 'require': 'skill-20'}, {'name': 'skill-1', 'offense': 3, 'points': 4, 'require': None}, {'name': 'skill-40', 'offense': 132, 'points': 25, 'require': 'skill-39'}, {'name': 'skill-33', 'offense': 70, 'points': 22, 'require': 'skill-32'}, {'name': 'skill-53', 'offense': 28, 'points': 15, 'require': 'skill-52'}, {'name': 'skill-31', 'offense': 56, 'points': 21, 'require': 'skill-30'}, {'name': 'skill-30', 'offense': 50, 'points': 21, 'require': 'skill-29'}, {'name': 'skill-55', 'offense': 39, 'points': 16, 'require': 'skill-54'}, {'name': 'skill-46', 'offense': 3, 'points': 6, 'require': 'skill-45'}, {'name': 'skill-12', 'offense': 35, 'points': 21, 'require': 'skill-11'}, {'name': 'skill-39', 'offense': 122, 'points': 25, 'require': 'skill-38'}, {'name': 'skill-25', 'offense': 24, 'points': 17, 'require': 'skill-24'}, {'name': 'skill-57', 'offense': 54, 'points': 17, 'require': 'skill-56'}, {'name': 'skill-43', 'offense': 164, 'points': 26, 'require': 'skill-42'}, {'name': 'skill-13', 'offense': 38, 'points': 22, 'require': 'skill-12'}, {'name': 'skill-11', 'offense': 32, 'points': 20, 'require': 'skill-10'}]}
 ]
