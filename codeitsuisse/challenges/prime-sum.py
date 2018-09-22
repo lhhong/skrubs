d8daa98366b4574a3ce2412e5ae3eb4dff77c185
icycoldx3 <adnakerh@gmail.com>
2018-09-22 10:44:54 +0800

    prime sums test

--- codeitsuisse/challenges/prime-sum.py
+++ codeitsuisse/challenges/prime-sum.py
@@ -1,7 +1,61 @@
-def evaluate(data):
-    inputValue = data.get("input")
-    primes = [inputValue%i != 0 for i in range(2, int(inputValue**0.5)+1)]
-    return primes
+import math
+from itertools import count, islice
 
+def evaluate(inputValue):
+    def isPrime(n):
+        return n > 1 and all(n%i for i in islice(count(2), int(math.sqrt(n)-1)))
 
-tests = [{ "input": 19 }]
+    if isPrime(inputValue):
+        return [inputValue]
+
+    primes = []
+    for possiblePrime in range(2, inputValue):
+        # Assume number is prime until shown it is not.
+        is_Prime = True
+        for num in range(2, possiblePrime):
+            if possiblePrime % num == 0:
+                is_Prime = False
+        if is_Prime:
+            primes.append(possiblePrime)
+
+    primes.append(1)
+    primes = sorted(primes, reverse = True)
+    currVal = inputValue
+    currList = []
+    n = 0
+
+    while currVal != 0:
+        currVal = inputValue
+        tempPrimes = primes[:]
+        del tempPrimes[:n]
+        for i in tempPrimes:
+            if currVal - i == 0:
+                currList.append(i)
+                return currList
+            elif currVal - i > 0:
+                currVal = currVal - i
+                currList.append(i)
+        n += 1
+
+    # def sum_of_primes(value):
+    #     primes = prime_sieve(value)
+    #     lo = 0
+    #     hi = len(primes) - 1
+    #     while lo <= hi:
+    #         prime_sum = primes[lo] + primes[hi]
+    #         if prime_sum < value:
+    #             lo += 1
+    #         else:
+    #             if prime_sum == value:
+    #                 yield primes[lo], primes[hi]
+    #             hi -= 1
+    # return
+
+tests = [
+    1,
+    6,
+    21,
+    100,
+    96,
+    21
+]
